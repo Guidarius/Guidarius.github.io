344c1b2276327bd193e4023896bb9ded485931cb
Gondolin <guidothescourge@gmail.com>
2018-10-09 12:24:49 -0500

    adding stars

--- assets/index.js
+++ assets/index.js
@@ -2,14 +2,14 @@
 
 
 
-var timer;
-var view = $(window); 
+// var timer;
+// var view = $(window); 
 
-$("section").height(view.height());
-view.on('resize',function(){
-    clearTimeout(timer);
-    timer = setTimeout(function(){
-        $("section").height(view.height());
-    }, 100);
+// $("section").height(view.height());
+// view.on('resize',function(){
+//     clearTimeout(timer);
+//     timer = setTimeout(function(){
+//         $("section").height(view.height());
+//     }, 100);
 
-});
+// });
