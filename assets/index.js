ea82a9983747c5435a75116aaf88504931d2afc8
Gondolin <guidothescourge@gmail.com>
2018-10-08 23:05:04 -0500

    another update

--- assets/index.js
+++ assets/index.js
@@ -6,13 +6,10 @@ var timer;
 var view = $(window); 
 
 $("section").height(view.height());
-$("section").width(view.width());
-
 view.on('resize',function(){
     clearTimeout(timer);
     timer = setTimeout(function(){
         $("section").height(view.height());
-        $("section").width(view.width());
     }, 100);
 
 });
